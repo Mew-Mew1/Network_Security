c11c1c6fc94b8767e7da475e99809d775f13eaad
Mew-Mew1 <adityarajsinghdigari@gmail.com>
2026-03-16 17:39:06 +0530

    Setting up the ML flow and dagshub

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -28,12 +28,12 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+dagshub.init(repo_owner='is24bm035', repo_name='Network_Security', mlflow=True)
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
+'''os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/is24bm035/Network_Security.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="is24bm035"
 os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
-
+'''
 
 
 
@@ -47,7 +47,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        mlflow.set_registry_uri("https://dagshub.com/is24bm035/Network_Security.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
@@ -59,7 +59,6 @@ class ModelTrainer:
             mlflow.log_metric("f1_score",f1_score)
             mlflow.log_metric("precision",precision_score)
             mlflow.log_metric("recall_score",recall_score)
-            mlflow.sklearn.log_model(best_model,"model")
             # Model registry does not work with file store
             if tracking_url_type_store != "file":
 
@@ -67,9 +66,9 @@ class ModelTrainer:
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+                mlflow.sklearn.log_model(best_model, name="model", registered_model_name="network_security_model")
             else:
-                mlflow.sklearn.log_model(best_model, "model")
+                mlflow.sklearn.log_model(best_model, name="model")
 
 
         
